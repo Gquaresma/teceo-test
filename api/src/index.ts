73abb4a029baa7811cccf5d52a6e3b46e04c7499
Gabriel Quaresma <fulco02@gmail.com>
2023-11-14 12:14:48 -0300

    initial commit

---
+++ api/src/index.ts
@@ -0,0 +1,15 @@
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+
+const port = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
