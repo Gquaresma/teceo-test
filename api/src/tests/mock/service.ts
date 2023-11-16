675687430ae8284302199adcece6e03d96dd8b7a
Gabriel Quaresma <fulco02@gmail.com>
2023-11-16 08:16:46 -0300

    test: service

--- api/src/tests/mock/service.ts
+++ api/src/tests/mock/service.ts
@@ -6,8 +6,7 @@ export const mockRepo = {
 	findMany: jest.fn().mockReturnValue([{ operation: 'findMany' }]),
 	findUnique: jest.fn().mockReturnValue({ operation: 'findUnique' }),
 	create: jest.fn().mockReturnValue({ operation: 'create' }),
-	update: jest.fn().mockReturnValue({ operation: 'update' }),
-	delete: jest.fn().mockReturnValue({ operation: 'delete' }),
+
 };
 
 export const makeSutService = (repo: any) => new SutService(repo);
